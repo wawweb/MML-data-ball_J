58328ceb9ee614eb80a27971c477aa6f18a8247b
wawatai <100764746+wawatai@users.noreply.github.com>
2023-11-23 11:13:51 +0800

    112--1123-2-J
    解決JS ANIMATE 的BUG 改用KEYFRAMES解決

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,53 +1,53 @@
-(() => {
-    let nums9 = $("main .bonusPoint .bonusPoint__nums li.nums__9")
+// (() => {
+//     let nums9 = $("main .bonusPoint .bonusPoint__nums li.nums__9")
 
-    var current = parseFloat(nums9.css("backgroundPositionY"));
+//     var current = parseFloat(nums9.css("backgroundPositionY"));
 
-    let active = false;
+//     let active = false;
 
-    let add = () => {
+//     let add = () => {
 
-        current += 10;
+//         current += 10;
 
-        nums9.animate({
-            backgroundPositionY: "+=13%"
-        }, 1000, function () {
-            nums9.animate({
-                backgroundPositionY: "-=3%"
-            }, 1000)
-        });
+//         nums9.animate({
+//             backgroundPositionY: "+=13%"
+//         }, 1000, function () {
+//             nums9.animate({
+//                 backgroundPositionY: "-=3%"
+//             }, 1000)
+//         });
 
-        if (current == 90) {
-            active = false;
-        }
-    }
+//         if (current == 90) {
+//             active = false;
+//         }
+//     }
 
-    let reduce = () => {
+//     let reduce = () => {
 
-        current -= 10;
+//         current -= 10;
 
-        nums9.animate({
-            backgroundPositionY: "-=13%"
-        }, 1000, function () {
-            nums9.animate({
-                backgroundPositionY: "+=3%"
-            }, 1000)
-        });
+//         nums9.animate({
+//             backgroundPositionY: "-=13%"
+//         }, 1000, function () {
+//             nums9.animate({
+//                 backgroundPositionY: "+=3%"
+//             }, 1000)
+//         });
 
-        if (current == 0) {
-            active = true;
-        }
-    }
+//         if (current == 0) {
+//             active = true;
+//         }
+//     }
 
-    setInterval(() => {
-        setTimeout(() => {
-            if (current <= 90 && active == false) {
-                reduce();
-            }
-            else if (current >= 0 && active == true) {
-                add();
-            }
-        }, 1000);
-    }, 2000);
+//     setInterval(() => {
+//         setTimeout(() => {
+//             if (current <= 90 && active == false) {
+//                 reduce();
+//             }
+//             else if (current >= 0 && active == true) {
+//                 add();
+//             }
+//         }, 1000);
+//     }, 2000);
     
-})();
+// })();
